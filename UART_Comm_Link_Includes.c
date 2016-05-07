62befda65bd8122d481f40a9f8938b96d083bdb4
oomwoo <camera@ovsy.com>
2016-05-06 20:39:53 -0700

    Moved comm functions into separate file, optimized

---
+++ UART_Comm_Link_Includes.c
@@ -0,0 +1,122 @@
+// UART library for VEX Cortex to communicate with another UART
+// Use it to connect VEX Cortex e.g. to Raspberry Pi
+
+unsigned int nXmitChars = 0;
+unsigned int nRecvChars = 0;
+TUARTs nCommPort = uartTwo;
+
+enum LinkCommand
+{
+	LINK_CMD_NONE,
+	LINK_CMD_MANUAL_CONTROL,
+	LINK_CMD_AUTONOMOUS_CONTROL,
+	LINK_CMD_START_CAPTURE,
+	LINK_CMD_STOP_CAPTURE,
+	LINK_CMD_FORGET = 253,
+	LINK_CMD_TERMINATE_AND_UPLOAD = 254,
+	LINK_CMD_TERMINATE = 255
+};
+
+const char toHex[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+void QueueUartChar(char value)
+{
+	nXmitChars++;
+	sendChar(nCommPort, value);
+	while (!bXmitComplete(uartOne))
+	{
+		sleep(0);
+	}
+}
+
+void SendUartCmd(char cmd, char value)
+{
+	char nibble;
+	QueueUartChar(cmd);
+
+	nibble = value >> 4;
+	QueueUartChar(toHex[nibble]);
+
+	nibble = value & 0x0f;
+	QueueUartChar(toHex[nibble]);
+}
+
+void SendUartLF()
+{
+	QueueUartChar(0x0a);	// Line feed
+}
+
+short GetUartChar()
+{
+	short rcvChar;
+
+	rcvChar = getChar(nCommPort);
+	if (rcvChar != -1)
+		nRecvChars++;
+
+	return rcvChar;
+
+}
+
+char GetUartCharWait()
+{
+	short rcvChar;
+
+	while (true)
+	{
+		rcvChar = GetUartChar();
+		if (rcvChar == -1)
+			sleep(0);
+		else
+			return (char) rcvChar;
+	}
+}
+
+bool isValidHexChar(char ch)
+{
+	return ((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'F'));
+}
+
+char Char2Nibble(char nibble)
+{
+	if (nibble <= '9')
+		return nibble - '0';
+
+	return nibble - 'A';
+}
+
+char GetUartCmd(char* value)
+{
+	short rcvChar;
+
+	while (true)
+	{
+		rcvChar = GetUartChar();
+		if (rcvChar == -1)
+			return 0;
+
+		char ch = (char) rcvChar;
+
+		// valid command?
+		if (ch >= 'a' && ch <= 'z' || ch > 'F' && ch <= 'Z')
+		{
+			char cmd = ch;
+
+			char val1 = GetUartCharWait();
+			if (!isValidHexChar(val1))
+				continue;
+
+			char val2 = GetUartCharWait();
+			if (!isValidHexChar(val2))
+				continue;
+
+			char ret = GetUartCharWait();
+			if (ret != '\n')
+				continue;
+
+			*value = Char2Nibble(val1) << 4 + Char2Nibble(val2);
+			return cmd;
+		}
+	}
+
+}
